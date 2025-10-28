5a826b72c86d59336ab7d42333a306f1399333d5
Cade Fair <cfair6976@sdsu.edu>
2025-10-27 19:25:57 -0700

    Completed To dos in Heap h: implemented push, pop, upheap, and downheap

--- heap.h
+++ heap.h
@@ -15,21 +15,74 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+
+        data[size++] = idx;
+        upheap(size-1, weightArr);
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+
+        if (size == 0) { // empty check
+            return -1;
+        }
+        int out = data[0];
+        data[0] = data[size-1];
+        size--;
+        if (size > 0 ) {
+            downheap(0, weightArr);
+        }
+
+        return out;
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+        while (pos > 0) {
+            int parent = (pos -1) / 2;
+
+            //Compare the weights
+            if (weightArr[data[pos]] >= weightArr[data[parent]]) {
+                break;
+            }
+
+            // Swap
+            int temp = data[pos];
+            data[pos] = data[parent];
+            data[parent] = temp;
+
+            // Move up
+            pos = parent;
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+        while (true) {
+            int left = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = pos;
+
+            // Find the smallest among pos, left, and right
+            if (left < size && weightArr[data[left]] < weightArr[data[smallest]]) {
+                smallest = left;
+            }
+
+            if (right < size && weightArr[data[right]] < weightArr[data[smallest]]) {
+            smallest = right;
+            }
+
+            if (smallest == pos) {
+                break; // Heap property satisfied
+            }
+
+            // Swap parent with the smallest child
+            int temp = data[pos];
+            data[pos] = data[smallest];
+            data[smallest] = temp;
+
+            // Continue down
+            pos = smallest;
+        }
     }
 };
 
